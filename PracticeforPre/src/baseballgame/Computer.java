32b3d4628d1d8968ff77ff524898e4056ed1a9ea
choiyeonseok <smr603@snu.ac.kr>
2019-11-28 03:51:31 +0900

    기능 : 중복체크하는 메소드 추가

--- PracticeforPre/src/baseballgame/Computer.java
+++ PracticeforPre/src/baseballgame/Computer.java
@@ -3,6 +3,29 @@ package baseballgame;
 
 public class Computer {
 
+	int[] comNums;
 	
+	Computer(){
+		comNums = new int[3];
+	}
 	
+	int getRandNum() {
+		return (int)(Math.random() * 10);
+	}
+	
+	int getExceptZero() {
+		int rand;
+		do {
+			rand = getRandNum();
+		}while(rand == 0);
+		
+		return rand;
+	}
+	
+	boolean dupliCheck(int[] comNums, int rand) {
+		for (int number : comNums) {
+			return number == rand;
+		}
+		return false;
+	}
 }
