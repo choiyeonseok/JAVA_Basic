797c517e721107f3e87a0d12424880ac2f0455a0
choiyeonseok <smr603@snu.ac.kr>
2019-11-28 03:25:10 +0900

    기능 : 컴퓨터 클래스 추가

--- PracticeforPre/src/baseballgame/Computer.java
+++ PracticeforPre/src/baseballgame/Computer.java
@@ -1,29 +1,8 @@
 package baseballgame;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Computer {
 
-	List<Integer> comNums;
 	
-	public Computer() {
-		comNums = new ArrayList<>();
-	}
-	
-	
-	void createNumbers() {
-		comNums.clear();
-		while (comNums.size() < 3) {
-			int tmp;
-			tmp = (int)(Math.random() * 10);
-			if (!comNums.contains(tmp) && tmp > 0)
-				comNums.add(tmp);
-		}
-	}
-	
-	void showNumbers() {
-		System.out.println(comNums);
-	}
 	
 }
