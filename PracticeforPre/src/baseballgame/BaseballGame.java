fe22d206d00a60c9bc4bb0af471740e1d3f41a50
choiyeonseok <smr603@snu.ac.kr>
2019-11-28 11:30:40 +0900

    기능 : 게임 진행 클래스 생성자 수정

--- PracticeforPre/src/baseballgame/BaseballGame.java
+++ PracticeforPre/src/baseballgame/BaseballGame.java
@@ -5,17 +5,14 @@ public class BaseballGame {
 
 	int strike;
 	int ball;
-
-	int[] comNums;
-	int[] playerNums;
 	
 	BaseballGame(){
 		Computer com = new Computer();
 		Player player = new Player();
-		
-		comNums = com.createNumbers();
-		playerNums = player.createNumbers();
+		com.createNumbers();
+		player.createNumbers();
 	}
+	
 	
 	
 }
