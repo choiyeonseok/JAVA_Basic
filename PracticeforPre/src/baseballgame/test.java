aa97d98f3df9c4cd153e6c2d29adfa77bf52f3a6
choiyeonseok <smr603@snu.ac.kr>
2019-11-28 12:49:56 +0900

    형식 : eclipse 포매터 적용

--- PracticeforPre/src/baseballgame/test.java
+++ PracticeforPre/src/baseballgame/test.java
@@ -1,24 +1,12 @@
 package baseballgame;
 
-import java.util.Arrays;
-
 public class test {
 
 	public static void main(String[] args) {
 
-//		Computer comp = new Computer();
-//		comp.createNumbers();
-//		comp.showInfo();
-//	
-//		Player player = new Player();
-//		player.createNumbers();
-//		System.out.println(Arrays.toString(player.playerNums));
-//		
-		
 		BaseballGame game = new BaseballGame();
 		game.play();
-		
-		
+
 	}
-	
+
 }
