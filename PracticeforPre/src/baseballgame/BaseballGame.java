6e676971d6f4e5869c492f37386564db09829208
choiyeonseok <smr603@snu.ac.kr>
2019-11-28 11:27:36 +0900

    기능 : 게임진행자 클래스 생성자 추가

--- PracticeforPre/src/baseballgame/BaseballGame.java
+++ PracticeforPre/src/baseballgame/BaseballGame.java
@@ -3,6 +3,19 @@ package baseballgame;
 
 public class BaseballGame {
 
+	int strike;
+	int ball;
+
+	int[] comNums;
+	int[] playerNums;
+	
+	BaseballGame(){
+		Computer com = new Computer();
+		Player player = new Player();
+		
+		comNums = com.createNumbers();
+		playerNums = player.createNumbers();
+	}
 	
 	
 }
