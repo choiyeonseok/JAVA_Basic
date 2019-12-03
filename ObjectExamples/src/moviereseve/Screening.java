f3027a2d6a383b45e1b93a47203553f3807ac692
choiyeonseok <smr603@snu.ac.kr>
2019-12-04 01:52:46 +0900

    Money 클래스 추가

--- ObjectExamples/src/moviereseve/Screening.java
+++ ObjectExamples/src/moviereseve/Screening.java
@@ -1,5 +1,29 @@
 package moviereseve;
 
+import java.time.LocalDateTime;
+
 public class Screening {
 
+	private Movie movie;
+	private int sequence; // ¼ø¹ø
+	private LocalDateTime whenScreened;
+	
+	public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+		this.movie = movie;
+		this.sequence = sequence;
+		this.whenScreened = whenScreened;
+	}
+	
+	public LocalDateTime getStartTime() {
+		return whenScreened;
+	}
+	
+	public boolean isSequence(int sequence) {
+		return this.sequence == sequence;
+	}
+	
+	public Money getMovieFee() {
+		return movie.getFee();
+	}
+	
 }
