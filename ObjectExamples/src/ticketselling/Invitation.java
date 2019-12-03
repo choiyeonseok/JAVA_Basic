88d29e7faadca95cc2528d608a1883586d9b84e1
choiyeonseok <smr603@snu.ac.kr>
2019-12-03 18:33:30 +0900

    ticket selling ver 1.0

---
+++ ObjectExamples/src/ticketselling/Invitation.java
@@ -0,0 +1,9 @@
+package ticketselling;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+
+	private LocalDateTime when;
+	
+}
---
+++ ObjectExamples/src/ticketselling/Theater.java
@@ -0,0 +1,5 @@
+package ticketselling;
+
+public class Theater {
+
+}
