e63da423de59446d890c3c5bebdc8e84a140950c
choiyeonseok <smr603@snu.ac.kr>
2019-12-12 22:09:35 +0900

    기능 : 로또 구매 및 번호 입력 구현

---
+++ ObjectExamples/src/lotto/Main.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Player player = new Player();
+		player.inputPurchaseAmount();
+		player.makeLottoSet();
+	}
+
+}
