44f7d89dc5787f17135a5c892bd7f82146b44cc4
choiyeonseok <smr603@snu.ac.kr>
2019-12-19 01:39:28 +0900

    Refactor extract moveRow method from run method

--- src/domain/Ladder.java
+++ src/domain/Ladder.java
@@ -15,21 +15,21 @@ public class Ladder {
     public int run(int nthOfPerson) {
         for (int i = 0; i < rows.length; i++) {
             int[] row = rows[i];
-
-            if (row[nthOfPerson] == 0) {  // 좌,우 이동 없이 바로 내려온다.
-                continue;
-            }
-
-            if (nthOfPerson - 1 >= 0) {   // 0 보다 큰 인덱스까지, 1 감소한 인덱스를 반환
-                int leftValue = row[nthOfPerson - 1];
-                if (leftValue == 1) {
-                    nthOfPerson -= 1;
-                    continue;
-                }
-            }
-            nthOfPerson += 1;
+            nthOfPerson = moveRow(nthOfPerson, row);
         }
         return nthOfPerson;
     }
 
+    private int moveRow(int nthOfPerson, int[] row) {
+        if (row[nthOfPerson] == 0) {  // 좌,우 이동 없이 바로 내려온다.
+            return nthOfPerson;
+        }
+        if (nthOfPerson - 1 >= 0) {   // 0 보다 큰 인덱스까지, 1 감소한 인덱스를 반환
+            int leftValue = row[nthOfPerson - 1];
+            if (leftValue == 1) {
+                return nthOfPerson - 1;
+            }
+        }
+        return nthOfPerson + 1;
+    }
 }
