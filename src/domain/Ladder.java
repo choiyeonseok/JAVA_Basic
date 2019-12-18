0825109d4782e770467b7b8d84d5c21fcd02e362
choiyeonseok <smr603@snu.ac.kr>
2019-12-19 03:28:05 +0900

    Verify Ladder class method

--- src/domain/Ladder.java
+++ src/domain/Ladder.java
@@ -3,15 +3,24 @@ package domain;
 class Ladder {
     private Row[] rows;
 
-    Ladder(int countOfRows, int noOfPerson) {
-        rows = new Row[countOfRows];
-        for (int i = 0; i < countOfRows; i++) {
+    Ladder(int height, int noOfPerson) {
+        if (height < 1) {
+            throw new IllegalArgumentException(String.format("사다리 높이는 1 이상이어야 합니다. 현재 값 : %d ", height));
+        }
+        rows = new Row[height];
+        for (int i = 0; i < height; i++) {
             rows[i] = new Row(noOfPerson);
         }
     }
 
-    void drawLine(int noOfRow, int startPosition) {
-        rows[noOfRow].drawLine(startPosition);
+    void drawLine(int height, int startPosition) {
+        if (height < 0) {
+            throw new IllegalArgumentException(String.format("사다리 높이는 1 이상이어야 합니다. 현재 값 : %d ", height));
+        }
+        if (height > rows.length - 1) {
+            throw new IllegalArgumentException(String.format("사다리 최대 높이를 넘어 섰습니다. 현재 값 : %d ", height));
+        }
+        rows[height].drawLine(startPosition);
     }
 
     int run(int nthOfPerson) {
