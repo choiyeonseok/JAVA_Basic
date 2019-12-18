25ce3fdf5d45fbd7003b6954d88d273962ac2113
choiyeonseok <smr603@snu.ac.kr>
2019-12-19 03:19:21 +0900

    Add verification of drawLine method

--- src/domain/Row.java
+++ src/domain/Row.java
@@ -21,10 +21,24 @@ public class Row {
     int[] persons;
 
     Row(int noOfPersons){
+        if (noOfPersons < 1) {
+            throw new IllegalArgumentException(String.format("사람 수는 한 명 이상이어야 합니다. 현재 값은 : %d,", noOfPersons));
+        }
         persons = new int[noOfPersons];
     }
 
     void drawLine(int startPosition) {
+        if (startPosition < 0) {
+            throw new IllegalArgumentException(String.format("시작 점은 0이상이어야 합니다. 현재 값은 : %d", startPosition));
+        }
+
+        if (startPosition >= persons.length - 1) {
+            throw new IllegalArgumentException(String.format("시작 점은 %d미만이어야 합니다. 현재 값은 : %d", persons.length - 1, startPosition));
+        }
+
+        if (persons[startPosition] == -1) {
+            throw new IllegalArgumentException("선을 그을 수 없는 위치입니다.");
+        }
         persons[startPosition] = Direction.RIGHT.getNo();
         persons[startPosition + Direction.RIGHT.getNo()] = Direction.LEFT.getNo();
     }
--- test/domain/RowTest.java
+++ test/domain/RowTest.java
@@ -5,7 +5,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RowTest {
+    @Test
+    public void noOfPersons() throws Exception {
+        try {
+            new Row(0);
+            fail("IllegalArgumentException 에러가 발생해야 한다.");
+        } catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+    }
 
+    @Test
+    public void startPositionWhenMinus() throws Exception {
+        try {
+            Row row = new Row(3);
+            row.drawLine(-1);
+            fail("IllegalArgumentException 에러가 발생해야 한다.");
+        } catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void startPositionWhenOverNoOfPersons() throws Exception {
+        try {
+            Row row = new Row(3);
+            row.drawLine(2);
+            fail("IllegalArgumentException 에러가 발생해야 한다.");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void drawLineWhenAlreadyDrawingPoint() throws Exception {
+        try {
+            Row row = new Row(3);
+            row.drawLine(0);
+            row.drawLine(1);
+            fail("IllegalArgumentException 에러가 발생해야 한다.");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
     @Test
     public void moveWhenNoLine() throws Exception {
         Row row = new Row(3);
